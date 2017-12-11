f9eb5dece8aded0a44a60796e6cefb1c7eca35b1
Simon Bobrov <simon.bobrov@windeln.de>
2017-12-11 09:32:38 +0100

    removed links

--- push.sh
+++ push.sh
@@ -1,10 +1,5 @@
 npm run build
-touch docs/.nojekyll
-ln -s docs/index.html 404.html
-ln -s docs/index.html docs/sortable-array
-ln -s docs/index.html docs/sortable-form-array
-ln -s docs/index.html docs/custom-options
-ln -s docs/index.html docs/multiple-lists
+cp docs/index.html docs/404.html
 git add -A
 git commit -m 'built'
 git push
